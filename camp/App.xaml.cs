6e57ac4848011bb6d584637ce7642b6e9b314acb
il4mb <durianbohong@gmail.com>
2024-01-19 00:42:23 +0700

    add locale id and en

--- camp/App.xaml.cs
+++ camp/App.xaml.cs
@@ -93,6 +93,26 @@ namespace kokal
         }
 
 
+
+        private void SetLanguageDictionary()
+        {
+            ResourceDictionary dict = new ResourceDictionary();
+            switch (Thread.CurrentThread.CurrentCulture.ToString())
+            {
+                case "en-US":
+                    dict.Source = new Uri("..\\Resources\\StringResources.xaml", UriKind.Relative);
+                    break;
+                case "fr-CA":
+                    dict.Source = new Uri("..\\Resources\\StringResources.fr-CA.xaml", UriKind.Relative);
+                    break;
+                default:
+                    dict.Source = new Uri("..\\Resources\\StringResources.xaml", UriKind.Relative);
+                    break;
+            }
+            this.Resources.MergedDictionaries.Add(dict);
+        }
+
+
         public static void OpenEdiText(string filename)
         {
 
